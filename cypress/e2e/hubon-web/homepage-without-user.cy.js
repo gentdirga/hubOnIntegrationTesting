b8e9e01875eae0cb28cfd273d2c57cdee7b2586f
Harkamal Singh <honey.rawal89@gmail.com>
2022-11-29 23:47:55 +0530

    added first test to verify logo

--- cypress/e2e/hubon-web/homepage-without-user.cy.js
+++ cypress/e2e/hubon-web/homepage-without-user.cy.js
@@ -14,35 +14,9 @@ context('HomePage Without User LoggedIn', () => {
     cy.visit(hostUrl)
   })
 
-  // it('has logo', () => {
-    
-  // })
-
-  // it('navigation header has Home Marketplace Catalog Transport etc links', () => {
-  //   cy.get('.site-upper-navbar').contains('Home')
-  //   cy.get('.site-upper-navbar').contains('Marketplace')
-  //   cy.get('.site-upper-navbar').contains('Catalog')
-  //   cy.get('.site-upper-navbar').contains('Transport')
-  //   cy.get('.site-upper-navbar').contains('About')
-  //   cy.get('.site-upper-navbar').contains('Events')
-  //   cy.get('.site-upper-navbar').contains('Social')
-  //   cy.get('.site-upper-navbar').contains('Invite Friends')
-  //   cy.get('.site-upper-navbar').contains('Blogs')
-  //   cy.get('.site-upper-navbar').contains('FAQ')
-
-  // })
-
-  // it ('have signup and login links', () => {
-  //   cy.get('div.login-signup a:first').should('have.text', 'Sign Up')
-  //   cy.get('div.login-signup a:first').should('have.attr', 'href', '/en/signup')
-  //   cy.get('div.login-signup a:last').should('have.text', 'Login')
-  //   cy.get('div.login-signup a:last').should('have.attr', 'href', '/en/login')
-  // })
-
-  // it('have search form elements', () => {
-  //   cy.get('.search-form').within(() => {
-  //     cy.get('input.howtogrow_comment_input').should('have.attr', 'placeholder', 'What are you looking for?')
-  //     cy.get('input.pac-target-input').should('have.attr', 'placeholder', 'Location')
-  //   })
-  // })
+  it('has logo', () => {
+    cy.get('div#root').within(() => {
+      cy.get('img').should('have.attr', 'alt').should('include','HubOn by Gardeneur')
+    })
+  })
 })
