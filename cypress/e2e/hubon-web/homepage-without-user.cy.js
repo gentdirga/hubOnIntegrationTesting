824684374f3eadc840bf517857d3745be5a5f315
Harkamal Singh <honey.rawal89@gmail.com>
2022-11-30 00:34:11 +0530

    home page tests

--- cypress/e2e/hubon-web/homepage-without-user.cy.js
+++ cypress/e2e/hubon-web/homepage-without-user.cy.js
@@ -15,8 +15,26 @@ context('HomePage Without User LoggedIn', () => {
   })
 
   it('has logo', () => {
-    cy.get('div#root').within(() => {
-      cy.get('img').should('have.attr', 'alt').should('include','HubOn by Gardeneur')
-    })
+    cy.get('[alt="HubON by Gardeneur"]')
+  })
+
+  it('top bar has ETA Calculator FAQ and About link', () => {
+    cy.get('button').contains('ETA Calculator')
+    cy.get('a').contains('FAQ')
+    cy.get('a').contains('About')
+  })
+
+  it('navigation header has Home Track Transport Get Transport etc links', () => {
+    cy.get('ul').contains('Home')
+    cy.get('ul').contains('Track Transport')
+    cy.get('ul').contains('Get Transport')
+    cy.get('ul').contains('Our Hubs')
+    cy.get('ul').contains('Login / Sign up')
+  })
+
+  it ('verify text', () => {
+    cy.get('h3').contains('Easy, Economical, Eco-Friendly Transport')
+    cy.get('p').contains('$6.99 flat fee / transport')
+    cy.get('a').contains('Try now for FREE')
   })
 })
