2cb620a6ba72788eb0b9f0d54c9c1526bccdb2dd
Harkamal Singh <honey.rawal89@gmail.com>
2022-12-30 10:07:15 +0530

    driver dashboard on admin panel

---
+++ cypress/e2e/hubon-web/admin-panel/driver-dashboard-page.cy.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+
+let uiURL = Cypress.env('CYPRESS_BASE_URL');
+
+if (uiURL == "" || uiURL === undefined) {
+  uiURL = Cypress.env('default_hubon_web_url');
+}
+
+const hostUrl = uiURL;
+
+const uniqueSeed = Date.now().toString();
+const getUniqueId = () => Cypress._.uniqueId(uniqueSeed);
+const couponCode = getUniqueId();
+
+context('Admin panel driver dashboard page', () => {
+
+  beforeEach(() => {
+    cy.visit(hostUrl+'admin/login')
+    cy.get('input[name="username"]').type(Cypress.env('default_admin_username'))
+    cy.get('input[name="password"]').type(Cypress.env('default_admin_password'))
+    
+    Cypress.Commands.add('confirmCaptcha', function () {
+      cy.get('iframe')
+        .first()
+        .then((recaptchaIframe) => {
+          const body = recaptchaIframe.contents()
+          cy.wrap(body).find('.recaptcha-checkbox-border').should('be.visible').click()
+        })
+    })
+
+    cy.confirmCaptcha
+    cy.get('button').contains('Login').click()
+    cy.url().should('contain', '/admin')
+  })
+
+  it('driver dashboard page', () => {
+    cy.contains('a', "Driver's Dashboard").click()
+    cy.intercept('GET', '/admin/v1/hub_groups/1').as('get_hub_group')
+    cy.contains('h3', "HubOn Driver's Dashboard")
+    cy.get('select[name="groups"]').select("1")
+    cy.wait('@get_hub_group').its('response.statusCode').should('eq', 200)
+  })
+})
