436a2df0f29422ac7d979d3dcb8fe9329229656a
Harkamal Singh <honey.rawal89@gmail.com>
2023-01-07 20:26:39 +0530

    basic tests for get transport

--- cypress/e2e/hubon-web/get-transport.cy.js
+++ cypress/e2e/hubon-web/get-transport.cy.js
@@ -9,16 +9,19 @@ if (uiURL == "" || uiURL === undefined) {
 
 const hostUrl = uiURL;
 
-context('Track Transport Page', () => {
+context('Get Transport Page', () => {
   before(() => {
     cy.visit(hostUrl)
   })
 	
-	it('load track transport page', () => {
-    cy.get('ul').contains('Track Transport').click()
-		cy.get('h3').contains('Track your transport')
+	it('load get transport page', () => {
+		cy.get('ul').contains('Get Transport').trigger('mouseover')
+		cy.get('a').contains('Send a package')
+		cy.get('a').contains('Receive a package')
+		cy.get('a').contains('Receive a package').click({force: true})
+		cy.get('h3').contains('Welcome back to Hub')
 		cy.get('p').contains('We will require a phone number validation for security purposes')
 		cy.get('input[name="identity"]')
-		cy.get('button').contains('Track Transport')
+		cy.get('button').contains('Login')
 	})
 })
